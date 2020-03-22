61397dc164b08ee78f5b6c4c0a319d095720bef8
失之时间 <49801226+SwordJack@users.noreply.github.com>
2020-03-22 12:59:04 +0800

    Add 11-1
    添加了第11章的第1个样例：飘动动画窗体。

---
+++ 第11章/例11-1/飘动动画窗体/WaveForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace 飘动动画窗体
+{
+    partial class WaveForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btPause = new System.Windows.Forms.Button();
+            this.btMove = new System.Windows.Forms.Button();
+            this.btVertical = new System.Windows.Forms.Button();
+            this.btHorizontal = new System.Windows.Forms.Button();
+            this.timerHorizontal = new System.Windows.Forms.Timer(this.components);
+            this.timerVertical = new System.Windows.Forms.Timer(this.components);
+            this.timerMove = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbTitle.Location = new System.Drawing.Point(201, 75);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.ReadOnly = true;
+            this.tbTitle.Size = new System.Drawing.Size(222, 44);
+            this.tbTitle.TabIndex = 0;
+            this.tbTitle.Text = "飘动动画窗体";
+            this.tbTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btPause);
+            this.groupBox1.Controls.Add(this.btMove);
+            this.groupBox1.Controls.Add(this.btVertical);
+            this.groupBox1.Controls.Add(this.btHorizontal);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(128, 155);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(369, 194);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "飘动动画窗体的控制";
+            // 
+            // btPause
+            // 
+            this.btPause.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btPause.Location = new System.Drawing.Point(217, 119);
+            this.btPause.Name = "btPause";
+            this.btPause.Size = new System.Drawing.Size(90, 40);
+            this.btPause.TabIndex = 3;
+            this.btPause.Text = "停止飘动";
+            this.btPause.UseVisualStyleBackColor = true;
+            this.btPause.Click += new System.EventHandler(this.btPause_Click);
+            // 
+            // btMove
+            // 
+            this.btMove.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btMove.Location = new System.Drawing.Point(61, 119);
+            this.btMove.Name = "btMove";
+            this.btMove.Size = new System.Drawing.Size(90, 40);
+            this.btMove.TabIndex = 2;
+            this.btMove.Text = "移动窗体";
+            this.btMove.UseVisualStyleBackColor = true;
+            this.btMove.Click += new System.EventHandler(this.btMove_Click);
+            // 
+            // btVertical
+            // 
+            this.btVertical.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btVertical.Location = new System.Drawing.Point(217, 36);
+            this.btVertical.Name = "btVertical";
+            this.btVertical.Size = new System.Drawing.Size(90, 40);
+            this.btVertical.TabIndex = 1;
+            this.btVertical.Text = "垂直移动";
+            this.btVertical.UseVisualStyleBackColor = true;
+            this.btVertical.Click += new System.EventHandler(this.btVertical_Click);
+            // 
+            // btHorizontal
+            // 
+            this.btHorizontal.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btHorizontal.Location = new System.Drawing.Point(61, 36);
+            this.btHorizontal.Name = "btHorizontal";
+            this.btHorizontal.Size = new System.Drawing.Size(90, 40);
+            this.btHorizontal.TabIndex = 0;
+            this.btHorizontal.Text = "水平移动";
+            this.btHorizontal.UseVisualStyleBackColor = true;
+            this.btHorizontal.Click += new System.EventHandler(this.BtHorizontal_Click);
+            // 
+            // timerHorizontal
+            // 
+            this.timerHorizontal.Tick += new System.EventHandler(this.TimerHorizontal_Tick);
+            // 
+            // timerVertical
+            // 
+            this.timerVertical.Tick += new System.EventHandler(this.TimerVertical_Tick);
+            // 
+            // timerMove
+            // 
+            this.timerMove.Tick += new System.EventHandler(this.TimerMove_Tick);
+            // 
+            // WaveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "WaveForm";
+            this.Text = "飘动动画窗体";
+            this.Load += new System.EventHandler(this.WaveForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btPause;
+        private System.Windows.Forms.Button btMove;
+        private System.Windows.Forms.Button btVertical;
+        private System.Windows.Forms.Button btHorizontal;
+        private System.Windows.Forms.Timer timerHorizontal;
+        private System.Windows.Forms.Timer timerVertical;
+        private System.Windows.Forms.Timer timerMove;
+    }
+}
+
---
+++ 第11章/例11-1/飘动动画窗体/WaveForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 飘动动画窗体
+{
+    public partial class WaveForm : Form
+    {
+
+        int screenWidth;    //屏幕宽度。
+        int screenHeight;   //屏幕高度。
+        Point formPosition; //当前窗体的位置。
+        
+        public WaveForm()
+        {
+            InitializeComponent();
+            screenWidth = SystemInformation.PrimaryMonitorMaximizedWindowSize.Width;    //最大化窗体的宽度作为屏幕宽度。
+            screenHeight = SystemInformation.PrimaryMonitorMaximizedWindowSize.Height;  //最大化窗体的高度作为屏幕高度。
+        }
+
+        private void WaveForm_Load(object sender, EventArgs e)
+        {
+            this.timerHorizontal.Enabled
+                = this.timerVertical.Enabled
+                = this.timerMove.Enabled = false;
+            this.formPosition = new Point(this.DesktopLocation.X, this.DesktopLocation.Y);   //获取当前窗体的位置。
+        }
+
+        /// <summary>
+        /// 每当timerHorizontal的tick事件触发时，窗体横坐标+1。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerHorizontal_Tick(object sender, EventArgs e)
+        {
+            this.formPosition.X = this.DesktopLocation.X;
+            this.formPosition.Y = this.DesktopLocation.Y;
+            if (formPosition.X + this.Width < this.screenWidth)
+            {
+                this.formPosition.X += 1;
+            }
+            else
+            {
+                formPosition.X = 0;
+            }
+            this.DesktopLocation = this.formPosition;
+        }
+
+        /// <summary>
+        /// 每当timerVertical的tick事件触发时，窗体纵坐标+1。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerVertical_Tick(object sender, EventArgs e)
+        {
+            this.formPosition.X = this.DesktopLocation.X;
+            this.formPosition.Y = this.DesktopLocation.Y;
+            if (formPosition.Y + this.Height < this.screenHeight)
+            {
+                this.formPosition.Y += 1;
+            }
+            else
+            {
+                formPosition.Y = 0;
+            }
+            this.DesktopLocation = this.formPosition;
+        }
+
+        /// <summary>
+        /// 每当timerMove的tick事件触发时，窗体位置横纵坐标按比例增加。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerMove_Tick(object sender, EventArgs e)
+        {
+            this.formPosition.X = this.DesktopLocation.X;
+            this.formPosition.Y = this.DesktopLocation.Y;
+            if (this.formPosition.X + this.Width < screenWidth 
+                && this.formPosition.Y + this.Height < screenHeight)
+            {
+                this.formPosition.X += 1;
+                this.formPosition.Y += 1;
+            }
+            else
+            {
+                this.formPosition.X = this.formPosition.Y = 0;
+            }
+            this.DesktopLocation = this.formPosition;
+        }
+
+        private void BtHorizontal_Click(object sender, EventArgs e)
+        {
+            timerHorizontal.Enabled = true;
+            timerVertical.Enabled = timerMove.Enabled = false;
+        }
+
+        private void btVertical_Click(object sender, EventArgs e)
+        {
+            timerVertical.Enabled = true;
+            timerHorizontal.Enabled = timerMove.Enabled = false;
+        }
+
+        private void btMove_Click(object sender, EventArgs e)
+        {
+            timerMove.Enabled = true;
+            timerHorizontal.Enabled = timerVertical.Enabled = false;
+        }
+
+        private void btPause_Click(object sender, EventArgs e)
+        {
+            timerHorizontal.Enabled = timerVertical.Enabled = timerMove.Enabled = false;
+        }
+    }
+}
